318146adab20eac5bcc908127153a52607b940cc
Ayron Felipe <ayron41@gmail.com>
2022-12-19 15:48:41 -0300

    feat: send a message to queue with success

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Write something very beautiful and inspiring here
+
+First run rabbitmq on your local machine:
+
+```bash
+docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3-management
+```
+
+To run the python file run:
+
+```
+poetry run python /path/to/your/file
+```
--- rabbitmq-learn/send.py
+++ rabbitmq-learn/send.py
@@ -2,3 +2,12 @@ import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("127.0.0.1"))
 channel = connection.channel()
+
+channel.queue_declare(queue="hello")
+
+body = "Hello World!"
+
+channel.basic_publish(exchange="", routing_key="hello", body=body)
+print(f" [x] Sent '{body}'")
+
+connection.close()
