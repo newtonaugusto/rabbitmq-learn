747725adbaf8d34a9d74391e5259be7509431c68
Ayron Felipe <ayron41@gmail.com>
2022-12-19 16:12:58 -0300

    chore: organize files in folders

--- rabbitmq-learn/tutorial-one/receive.py
+++ rabbitmq-learn/tutorial-one/receive.py
@@ -1,4 +1,7 @@
-import pika, sys, os
+import os
+import sys
+
+import pika
 
 
 def callback(ch, method, properties, body):
@@ -10,18 +13,18 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters("127.0.0.1"))
     channel = connection.channel()
 
+    channel.queue_declare(queue="hello")
 
-    channel.basic_consume(queue='hello',
-                        auto_ack=True,
-                        on_message_callback=callback)
-    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.basic_consume(queue="hello", auto_ack=True, on_message_callback=callback)
+    print(" [*] Waiting for messages. To exit press CTRL+C")
     channel.start_consuming()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('Interrupted')
+        print("Interrupted")
         try:
             sys.exit(0)
         except SystemExit:
